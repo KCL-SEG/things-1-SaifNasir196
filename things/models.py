9ff7351c2a07c48e43f4bc205a7777e9734b4a54
Saif <muhammadsaif713@gmail.com>
2023-12-07 01:24:08 +0000

    complete exercise 2

--- things/models.py
+++ things/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Thing(models.Model):
+    
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
