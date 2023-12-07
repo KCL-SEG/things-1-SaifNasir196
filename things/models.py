cffaca80fda56bbe600e973a00ec6c7780de17e5
Saif <muhammadsaif713@gmail.com>
2023-12-07 01:26:14 +0000

    complete exercise 3

--- things/models.py
+++ things/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Thing(models.Model):
     
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
     description = models.TextField(max_length=250)
     quantity = models.IntegerField()
 
